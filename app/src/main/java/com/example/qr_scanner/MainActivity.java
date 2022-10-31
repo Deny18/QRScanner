42bb59ba2f3811ba5a2f4c2167bcf5699a76ad66
Deny18 <denyrahmatullah98@gmail.com>
2022-10-31 07:05:50 +0700

    QR Code Scanner

---
+++ app/src/main/java/com/example/qr_scanner/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.qr_scanner;
+
+import static com.example.qr_scanner.R.color.teal_200;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
